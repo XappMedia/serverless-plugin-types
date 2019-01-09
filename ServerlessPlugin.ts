09edeeaf6b9bb9c64ea9f3f1b4591e90e2742924
Chris <chris@xappmedia.com>
2019-01-09 10:55:16 -0500

    feat(hooks): Adding before:aws:deploy:deploy:createStack

--- ServerlessPlugin.ts
+++ ServerlessPlugin.ts
@@ -1,8 +1,9 @@
 export type ObjOrPromise<T> = T | Promise<T>;
 
 export interface Hooks {
-    "before:aws:deploy:deploy:updateStack"?: () => ObjOrPromise<void>;
-    "after:aws:deploy:deploy:updateStack"?: () => ObjOrPromise<void>;
+    "before:aws:deploy:deploy:createStack"?: () => ObjOrPromise<any>;
+    "before:aws:deploy:deploy:updateStack"?: () => ObjOrPromise<any>;
+    "after:aws:deploy:deploy:updateStack"?: () => ObjOrPromise<any>;
 }
 
 export interface ServerlessPlugin {
